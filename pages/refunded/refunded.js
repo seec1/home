805e8fdc20a5a0951206f66f1aa3095c3d9b828c
EricZhang809 <ericzhang809@outlook.com>
2019-07-22 09:22:38 +0800

    finish refund page style

--- pages/refunded/refunded.js
+++ pages/refunded/refunded.js
@@ -1,34 +1,11 @@
-// pages/refunding/refunding.js
+// pages/refunded/refunded.js
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
-    show: false,
-    cur:0,
-    items:[
-      {
-        name:'',
-        value:'拍错/多拍/不想要',
-        index:0
-      },
-      {
-        name: '',
-        value: '协商一致退款',
-        index: 1
-      },
-      {
-        name: '',
-        value: '未按约定时间发货',
-        index:2
-      },
-      {
-        name: '',
-        value: '其他',
-        index:3
-      }
-    ]
+
   },
 
   /**
@@ -36,33 +13,17 @@ Page({
    */
   onLoad: function (options) {
     wx.setNavigationBarTitle({
-      title:'申请退款'
+      title: '退款成功',
     })
   },
+
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
   onReady: function () {
 
   },
-  checkReason(){
-    this.setData({
-      show:true
-    })
-  },
-  onClose() {
-    this.setData({ show: false });
-  },
-  checkedItem(e){
-    this.setData({
-      cur: e.target.dataset.index
-    })
-  },
-  toRefunded(){
-    wx.navigateTo({
-      url: '../refunded/refunded',
-    })
-  },
+
   /**
    * 生命周期函数--监听页面显示
    */
@@ -80,9 +41,10 @@ Page({
   /**
    * 生命周期函数--监听页面卸载
    */
-  uploader: function () {
+  onUnload: function () {
 
   },
+
   /**
    * 页面相关事件处理函数--监听用户下拉动作
    */
